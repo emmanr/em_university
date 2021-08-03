553dac3f220ce5f00bcb67350a91c70a9537f834
Emman Ruaza <emmanuel.ruaza@bibo.com.ph>
2021-08-04 02:33:43 +0800

    added custom root path for logged in users

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   devise_for :students, controllers: { confirmations: 'confirmations' }
   get 'students', to: "students#index"
 
-  root to: "pages#index"
+  devise_scope :student do
+    authenticated do
+      root to: 'courses#index'
+    end
+    unauthenticated do
+      root to: "pages#index", as: :unauthenticated_root
+    end
+  end
 end
