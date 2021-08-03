1304d6a7eb0a198f9fa85038d0c37bd9000756ff
Emman Ruaza <emmanuel.ruaza@bibo.com.ph>
2021-08-04 02:05:55 +0800

    added redirect to after user confirm email

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :students
+  devise_for :students, controllers: { confirmations: 'confirmations' }
   get 'students', to: "students#index"
 
   root to: "pages#index"
