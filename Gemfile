60d1263d1aa99aef96bbe0b5c879c8039b9b0124
Emman Ruaza <emmanuel.ruaza@bibo.com.ph>
2021-08-06 14:24:26 +0800

    added pg gem and database

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.0'
 
 gem 'rails', '~> 6.1.4'
+gem 'pg'
 gem 'puma', '~> 5.0'
 gem 'sass-rails', '>= 6'
 gem 'webpacker', '~> 5.0'
@@ -25,10 +26,6 @@ gem 'bootsnap', '>= 1.4.4', require: false
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-end
-
-group :production do
-  gem 'pg'
 end
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,35 @@
 
 ActiveRecord::Schema.define(version: 2021_08_05_091933) do
 
-# Could not dump table "students" because of following StandardError
-#   Unknown type 'inet' for column 'current_sign_in_ip'
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "students", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.integer "failed_attempts", default: 0, null: false
+    t.datetime "locked_at"
+    t.string "unlock_token"
+    t.index ["confirmation_token"], name: "index_students_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_students_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_students_on_unlock_token", unique: true
+  end
 
 end
